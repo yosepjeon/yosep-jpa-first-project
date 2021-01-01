a2e4d4fc356a9b4c35a8521a58366c1ce31bf61e
yosepjeon <enekelx1@naver.com>
2021-01-02 01:15:57 +0900

    프로젝트 구조 구성 완료

---
+++ src/main/java/com/yosep/spring/YosepJpaApplication.java
@@ -0,0 +1,12 @@
+package com.yosep.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YosepJpaApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(YosepJpaApplication.class, args);
+    }
+}
