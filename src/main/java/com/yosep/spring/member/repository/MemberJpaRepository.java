ae4d0fe8274d514e24d4800013a89a1f9d4e74a4
yosepjeon <enekelx1@naver.com>
2021-01-04 00:05:40 +0900

    jpa 추가

---
+++ src/main/java/com/yosep/spring/member/repository/MemberJpaRepository.java
@@ -0,0 +1,7 @@
+package com.yosep.spring.member.repository;
+
+import com.yosep.spring.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberJpaRepository extends JpaRepository<Member ,String> {
+}
